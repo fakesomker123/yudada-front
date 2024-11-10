a0a74bdb7639720f93154242c81fadc2211958da
ItFakeSmoker <14240619+it-fakesmoker@user.noreply.gitee.com>
2024-11-11 00:18:28 +0800

    前端项目第一次部署

--- yudada-frontend/src/request.ts
+++ yudada-frontend/src/request.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 
-const myAxios = axios.create({
-  baseURL: "http://localhost:8101",
-  timeout: 60000,
-  withCredentials: true,
-});
+// 是否是开发环境
+export const isDev = process.env.NODE_ENV === "development";
 
+// 创建 Axios 实例
+const myAxios = axios.create({
+    baseURL: isDev ? "http://localhost:8101" : "https://aidawn-127885-8-1331430179.sh.run.tcloudbase.com/",
+    timeout: 60000,
+    withCredentials: true,
+});
 // 全局请求拦截器
 myAxios.interceptors.request.use(
   function (config) {
